70366540f2f5c2eb705099aefa90309140c4e872
Hannes Ylä-Jääski <hanski@drg.fi>
2021-05-17 14:06:11 +0300

    build.sh: added time command

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ export SUBARCH=arm64
 
 echo "Starting to build.."
 make O=out ARCH=arm64 handeli_defconfig
-PATH=/home/drgreen/android/clang-r407598b/bin:/home/drgreen/:/home/drgreen/android/toolchain/bin:${PATH} make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android-
+PATH=/home/drgreen/android/clang-r407598b/bin:/home/drgreen/:/home/drgreen/android/toolchain/bin:${PATH} time make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android-
 echo "Build done."
